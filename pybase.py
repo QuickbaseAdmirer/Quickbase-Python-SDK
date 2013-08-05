d79a5e9490fc6471488edd6cac298d0f59e1f90a
Nick Hynes <nhynes@MTVL11407f12a.local>
2013-08-05 15:14:19 -0700

    Added documentation to code and README
    Added basic documentation and code comments that describe the included
    functions

--- pybase.py
+++ pybase.py
@@ -1,7 +1,7 @@
-#!/usr/local/bin/python3.3
+#!/usr/bin/python
 
-import urllib.request, urllib.parse, urllib.error
-#import urllib		#Use this line instead of the previous for Python < 3.0
+#import urllib.request, urllib.parse, urllib.error #Use this for Python > 3
+import urllib		#Use this line instead of the previous for Python < 3.0
 import xml.etree.ElementTree as elementree
 import re
 import string
@@ -12,8 +12,8 @@ class QBConn:
 		self.token = token
 		self.appid = appid
 		self.ticket = None
-		self.realm = realm
-		self.error = 0
+		self.realm = realm	#This allows one QuickBase realm to proxy for another
+		self.error = 0		#Set after every API call. A non-zero value indicates an error. A negative value indicates an error with this library
 		self.tables = {}
 
 	def authenticate(self,username,password):
@@ -25,14 +25,18 @@ class QBConn:
 			self.ticket = resp.find("ticket").text
 			self.tables = self._getTables()
 
+	#Adds the appropriate fields to the request and sends it to QB
+	#Takes a dict of parameter:value pairs and the url extension (main or your table ID, mostly)
 	def request(self,params,url_ext):
 		url = self.url
 		url += url_ext
 		params['ticket'] = self.ticket
 		params['apptoken'] = self.token
 		params['realmhost'] = self.realm
-		urlparams = urllib.parse.urlencode(params)		#urllib.urlencode(params)
-		resp = urllib.request.FancyURLopener().open(url+"?"+urlparams).read()	#urllib.FancyURLopener().open(url+"?"+urlparams).read()
+		#urlparams = urllib.parse.urlencode(params) #Use this line for Python > 3
+		urlparams = urllib.urlencode(params)	#use this line for < Python 3
+		#resp = urllib.request.FancyURLopener().open(url+"?"+urlparams).read() #Use this line for Python > 3
+		resp = urllib.FancyURLopener().open(url+"?"+urlparams).read() #use this line for < Python 3
 		if re.match('^\<\?xml version=',resp.decode("utf-8")) == None:
 			print("No useful data received")
 			self.error = -1		#No XML data returned
@@ -41,6 +45,9 @@ class QBConn:
 			self.error = int(tree.find('errcode').text)
 			return tree
 
+	#Creates a record with the given data in the table specified by tableID
+	#Takes a tableID (you can get this using qb.tables["yourtable"])
+	#Also takes a dict containing field name:field value pairs
 	def addRecord(self,tableID,data):
 		fields = self.getFields(tableID)
 		params = {'act':'API_AddRecord'}
@@ -49,6 +56,8 @@ class QBConn:
 					params["_fid_"+fields[field]] = data[field]
 		return self.request(params,tableID)
 
+	#Updates a reord with the given data
+	#Takes the record's table ID, record ID, a dict containing field:newvalue pairs, and an optional dict with param:value pairs
 	def editRecord(self,tableID,rid,newdata,options={}):
 		params = {'act':'API_EditRecord','rid':rid}
 		fields = self.getFields(tableID)
@@ -61,14 +70,18 @@ class QBConn:
 		params = dict(params,**options)
 		return self.request(params,tableID)
 
+	#Deletes the record specified by rid from the table given by tableID
 	def deleteRecord(self,tableID,rid):
 		params = {'act':'API_DeleteRecord','rid':rid}
 		return self.request(params,tableID)
 
+	#Deletes every record from tableID selected by query
 	def purgeRecords(self,tableID,query):
 		params = {'act':'API_PurgeRecords','query':query}
 		return self.request(params,tableID)
 
+	#Returns a dict containing fieldname:fieldid pairs
+	#Field names will have spaces replaced with not spaces
 	def getFields(self,tableID):
 		params = {'act':'API_GetSchema'}
 		schema = self.request(params,tableID)
@@ -79,6 +92,8 @@ class QBConn:
 			fieldlist[label] = field.attrib['id']
 		return fieldlist
 
+	#Returns a dict of tablename:tableID pairs
+	#This is called automatically after successful authentication
 	def _getTables(self):
 		if self.appid == None:
 			return {}
@@ -90,6 +105,8 @@ class QBConn:
 			tables[chdb.attrib['name'][6:]] = chdb.text
 		return tables
 
+	#Executes a query on tableID
+	#Returns a list of dicts containing fieldname:value pairs. record ID will always be specified by the "rid" key
 	def query(self,tableID,query):
 		params = dict(query)
 		params['act'] = "API_DoQuery"
@@ -107,6 +124,11 @@ class QBConn:
 			data.append(temp)
 		return data
 
+	#Emulates the syntax of basic (SELECT,DELETE) SQL queries
+	#Example: qb.sql("SELECT * FROM users WHERE name`EX`John\_Doe OR role`EX`fakeperson") #The \_ represents a space. This is a very basic function that doesn't use state machines. Note: field and table names will not have spaces
+	#Example: qb.sql("SELECT firstname|lastname FROM users WHERE paid`EX`true ORDER BY lastname ASC LIMIT 100")
+	#Example: qb.sql("DELETE FROM assets WHERE value`BF`0")
+	#I encourage you to modify this to suit your needs. Please contribute this back to the Python-QuickBase-SDK repository. Give QuickBase the API it deserves...
 	def sql(self,querystr):
 		tokens = querystr.split(" ")
 		if tokens[0] == "SELECT":
@@ -125,7 +147,7 @@ class QBConn:
 					for i in range(where+1,len(tokens)):
 						if (i-where+1)%2 == 0:
 							filt = tokens[i].split("`")
-							querystr += "{'"+tfields[filt[0]]+"'."+filt[1]+".'"+filt[2]+"'}"
+							querystr += "{'"+tfields[filt[0]]+"'."+filt[1]+".'"+filt[2].replace("\_"," ")+"'}"
 						elif tokens[i] == "AND" or tokens[i] == "OR":
 							querystr += tokens[i]
 						else:
@@ -149,7 +171,7 @@ class QBConn:
 				except ValueError:
 					pass
 				except:
-					print("SQL error near LIMIT")
+					print("SQL error near ORDER")
 					self.error = -2
 					return
 
@@ -183,7 +205,6 @@ class QBConn:
 				else:
 					break
 			return self.purgeRecords(tid,querystr)
-
 
 
 
